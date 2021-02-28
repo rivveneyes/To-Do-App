fe89b7ae60dfd80a00cde8fd6fc3210485eb1d2f
rivveneyes <garciajoshua907@yahoo.com>
2021-02-27 18:27:57 -0800

    completed removable task and strickthru font along with bottom panel buttons added with partial functionality

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const button=document.getElementById("button");
 const list= document.querySelector(".list");
 const addedItem = document.querySelector("input");
-
+const panal=document.getElementsByClassName("panel")[0]
 
 button.addEventListener("click" ,e=>{
-   let item=document.createElement("li");
-   let remover = document.createElement("button");
+   var item=document.createElement("li");
+   var remover = document.createElement("button");
+   var completer = document.createElement("input");
    remover.type="button";
-   remover.innerText="button"
-   console.log(remover);
+   remover.className="delete";
+   remover.innerText="remove";
+
+   completer.type="checkbox";
+   completer.className="complete";
+
+
+   console.log(completer);
+
    item.innerText=addedItem.value;
+   item.className="listed-item";
    item.appendChild(remover);
+   item.appendChild(completer)
    console.log(item);
    list.appendChild(item)
    
@@ -19,3 +29,33 @@ button.addEventListener("click" ,e=>{
 
 
 
+
+list.addEventListener("click", e=>{
+   var option= e.target;
+   if(option.className == "delete"){
+      list.removeChild(option.parentNode)
+      }
+   if(option.className =="complete"){
+      if(option.checked){
+      option.parentNode.style.textDecoration ="line-through";
+   }
+   else{
+      option.parentNode.style.textDecoration="none";
+   }
+   }
+
+})
+
+
+panal.addEventListener("click", e=>{
+   var option = e.target;
+   var listItems=document.getElementsByClassName("listed-item")
+   
+   if(option.innerText=="clear"){   
+   for(let i=listItems.length-1;i>=0;i--){
+      list.removeChild(listItems[i]);
+   }}
+   if(option.innerText=="finished"){
+      console.log("TAG");     
+}
+});
